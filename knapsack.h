adbca119dc509e1d7c9ca6d98b72cc172fb506eb
sushil_lakra <sushillakra21@gmail.com>
2019-08-01 12:36:51 +0530

    knapsack main

--- knapsack.h
+++ knapsack.h
@@ -44,3 +44,27 @@ int knapsack(int v[], int w[], int n, int c, int** aux)
 
     return aux[n][c];
 }
+
+/*
+int main()
+{
+    int v[] = {60, 100, 120, 200};
+    int w[] = {1, 2, 3, 4};
+
+    int m = sizeof(v) / sizeof(v[0]);
+    int n = 5;
+
+    int** aux = new int*[m + 1];
+    for (int i = 0; i < m + 1; ++i)
+        aux[i] = new int[n + 1];
+
+    for (int i = 0; i < m + 1; ++i)
+        for (int j = 0; j < n + 1; ++j)
+            aux[i][j] = 0;
+
+    std::cout << knapsack(v, w, m, n, aux) << std::endl;
+
+    int stop;
+    std::cin >> stop;
+}
+*/
